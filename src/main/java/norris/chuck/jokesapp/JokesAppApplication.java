2583bc7d2cc7f8867ad38e7ede928fe48396956f
hinampudi <hinampudi2021@my.fit.edu>
2023-01-16 01:41:13 -0600

    Initial Commit

---
+++ src/main/java/norris/chuck/jokesapp/JokesAppApplication.java
@@ -0,0 +1,13 @@
+package norris.chuck.jokesapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JokesAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JokesAppApplication.class, args);
+    }
+
+}

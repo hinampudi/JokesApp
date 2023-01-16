79a86c211d607ed479b05aa45c2eee3705eaec75
hinampudi <hinampudi2021@my.fit.edu>
2023-01-16 02:01:52 -0600

    Created Controllers and Index.html

---
+++ src/main/java/norris/chuck/jokesapp/controllers/JokeController.java
@@ -0,0 +1,18 @@
+package norris.chuck.jokesapp.controllers;
+
+import norris.chuck.jokesapp.services.JokeService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+@Controller
+public class JokeController {
+    private final JokeService jokeService;
+    public JokeController(JokeService jokeService) {
+        this.jokeService = jokeService;
+    }
+    @RequestMapping({"/"," "})
+    public String showJoke(Model model){
+        model.addAttribute("joke", jokeService.getJoke());
+        return "index";
+    }
+}
